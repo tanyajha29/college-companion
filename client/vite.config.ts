ad48e40b4957cab0d135ba698d79ffd8f381b96e
tanyajha29 <jhatanya211@gmail.com>
2026-04-01 22:58:13 +0530

    fix: update API proxy target to remote server address

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+
   // ✅ ADD THIS SERVER CONFIGURATION
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Change if your backend runs on a different port
+        target: 'http://15.207.59.143:5000', // Change if your backend runs on a different port
         changeOrigin: true,
         secure: false,      
       },
